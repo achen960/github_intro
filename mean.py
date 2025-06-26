0c5e89e3606cf407790f2d77bb7e9334af76838b
Austin <austinxchen02@gmail.com>
2025-06-25 22:00:15 -0700

    Add median function

--- mean.py
+++ mean.py
@@ -7,3 +7,12 @@ def mean(input):
 
 my_list = [0, 1, 2, 3, 4, 5]
 print(mean(my_list))
+import math
+def median(input):
+  mid = math.floor(len(input)/2)
+  input.sort()
+  if len(input) % 2 == 0:
+    return (input[mid] + input[mid - 1])/2
+  else:
+    return input[mid]
+print(median(my_list))
